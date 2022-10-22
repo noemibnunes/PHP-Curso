bafe778e7e1ed7321077ed762b3fcaaf7dd65527
Noemi Bezerra <noemibezerra1@gmail.com>
2022-10-22 20:53:43 -0300

    data e web - php

---
+++ 12_datas/1_date/index.php
@@ -0,0 +1,21 @@
+<?php
+
+// date('d/m/y') - dia, mês e ano (formatar data atual)
+
+// date('D, M - Y') - dia(nome), mês(nome) e ano (formatar data atual)
+
+// date('d/m/y', strtotime('+2 years')) - 2 anos a mais
+
+// mktime(hora, minuto, segundo, mes, dia, ano) - a partir disso gera uma data
+
+// DateTime - tratar a data como objeto
+    // format - formata a data
+    // modify - altera a data
+
+// setDate - recebe ano, mes e dia alterando a data 
+
+// setTime - recebe hora, minuto e segundo alterando o tempo da data
+
+// diff - diferenca entre duas datas
+
+// date_default_timezone_set - alterar o fuso horario
---
+++ 13_php_web/1_web/index.php
@@ -0,0 +1,12 @@
+
+// $_ENV - variavel de ambiente
+
+// $_GET - parametros que foram enviados por um GET
+
+// $_POST - parametros que foram enviados por um POST
+
+// $_COOKIE - Valores do cookies
+
+// $_SERVER - inf sobre o servidor
+
+// $_FILES - arquivos que vieram por upload
