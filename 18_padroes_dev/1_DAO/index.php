7867c856355500dfa164d56bc916f54749820b97
Noemi Bezerra <56655281+noemibnunes@users.noreply.github.com>
2022-11-23 10:16:50 -0300

    anotacoes_dao

---
+++ 18_padroes_dev/1_DAO/index.php
@@ -0,0 +1,8 @@
+<?php
+    // DAO - Data Access Object;
+
+    // padrao de codigo utilizado para persistencia de dados, é uma classe responsavel pelas interacoes no BD
+    // separa regra de negocios da interacao com o banco de dados
+
+    // pode ser criada uma interface para o DAO - definindo os metodos (esqueleto para implementar a classe)
+    // os principais metodos sao os que constituem o CRUD
